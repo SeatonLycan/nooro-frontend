ffc7d4bc598b645a40b5beddddb267cd80371f96
Seaton Lycan <slycan42@gmail.com>
2025-02-14 13:38:20 -0700

    Update task type

--- types/task.d.ts
+++ types/task.d.ts
@@ -1,7 +1,12 @@
 export interface Task {
   id: number;
   title: string;
-  description: string;
+  color: string;
   completed: boolean;
   createdAt: string;
+}
+
+export interface NewTask {
+  title: string;
+  color: string;
 }
