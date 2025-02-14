30832d1a1a9751db64b709d862f368dac688a4ed
Seaton Lycan <slycan42@gmail.com>
2025-02-14 13:38:58 -0700

    Fix layout styling

--- src/app/components/Layout.tsx
+++ src/app/components/Layout.tsx
@@ -5,7 +5,7 @@ import Header from './Header';
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <main className="min-h-screen flex flex-col bg-background">
+      <main className="min-h-screen flex flex-col items-center bg-background">
         <Header />
         {children}
       </main>
