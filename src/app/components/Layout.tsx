7fcae581b746a7d8ef8c7c78d1a85c0d2a5b5fc3
Seaton Lycan <slycan42@gmail.com>
2025-02-14 10:44:31 -0700

    Create Layout component

---
+++ src/app/components/Layout.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import Header from './Header';
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <main className="min-h-screen flex flex-col bg-background">
+        <Header />
+        {children}
+      </main>
+    </>
+  )
+}
